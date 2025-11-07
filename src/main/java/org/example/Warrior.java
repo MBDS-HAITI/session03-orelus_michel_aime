697478331c299dedb6a887981140c44af355092e
jorelus <50710502+jorelus@users.noreply.github.com>
2025-11-06 21:51:05 -0500

    commit moa

--- src/main/java/org/example/Warrior.java
+++ src/main/java/org/example/Warrior.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Warrior extends Character implements Attacker {
+public class Warrior extends Character {
 
     public Warrior(int hpMax, Weapon weapon, String typeName, String name) {
         super(hpMax, weapon, typeName, name);
