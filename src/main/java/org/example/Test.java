ab9731460b55db10dd6d635cc49d00928af665c9
aime-ak <aimehuguens4@gmail.com>
2025-11-07 13:46:52 -0500

    Ajout de mon travail sur Aibranche

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Test {
+    private String nom;
+    private String prenom;
+    private  int age;
+
+    public Test(String nom, String prenom, int age) {
+        this.nom=nom;
+        this. prenom=prenom;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public  int getAge() {
+        return 1;
+    }
+
+    }
+
+
